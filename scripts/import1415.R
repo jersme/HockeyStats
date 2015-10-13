b0913628b4beeef4f0574eba7464c5625f227c08
Jeroen <jersme@gmail.com>
2015-10-13 10:59:26 +0200

    Added capitals only

--- scripts/import1415.R
+++ scripts/import1415.R
@@ -4,8 +4,20 @@
 library(nhlscrapr)
 library(dplyr)
 
+## load games
+all.games <- full.game.database()
+
+these.games <- all.games %>%
+  filter(season == "20142015",
+         hometeam == "WSH" | awayteam == "WSH")
+
 
 ## download and process all games in season 1415
-compile.all.games(season = "20142015", 
-                  rdata.folder = "games/downloads/s1415/", 
-                  wait = .5)
+compile.all.games(new.game.table = these.games, 
+                  wait = 0, 
+                  rdata.folder = "games/downloads/s1415/caps/input/", 
+                  output.folder = "games/downloads/s1415/caps/output/", 
+                  verbose = T)
+
+## load data in environment
+load("nhlscrapr-probs.RData")
